72a5b5a3cda56757c3bb132b28fc216bf7c86378
fanfank <reetsee@qq.com>
2014-11-15 11:38:06 +0800

    Added Reetsee_Http

---
+++ Reetsee/Http.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * @author xuruiqi
+ * @date   20141115
+ * @desc   Http类
+ */
+class Reetsee_Http {
+    public static function get($strField, $default = NULL) {
+        $value = $_GET[$strField];
+
+        if (NULL === $value) {
+            $value = $_POST[$strField];
+        }
+
+        if (NULL === $value) {
+            $value = $default;
+        }
+
+        return $value;
+    }
+}
