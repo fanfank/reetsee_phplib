558aa10b4c1a92193cc4243cfcdb5c48bc9424cb
fanfank <reetsee@qq.com>
2015-04-07 01:26:30 +0800

    Add ui input error code

--- Reetsee/Error.php
+++ Reetsee/Error.php
@@ -9,12 +9,16 @@ class Reetsee_Error {
     const ERR_SUCCESS = 0;
     const ERR_INVALID_FIELD = 1000;
     const ERR_FIELD_NOT_SET = 1001;
+    const ERR_SERVICE_ERROR = 1002;
+    const ERR_UI_INPUT_ERROR = 1003;
 
     //error messages
     protected static $_arrErrcode2Errmsg = array(
         self::ERR_SUCCESS       => 'Success',    
         self::ERR_INVALID_FIELD => 'Invalid field in array',
         self::ERR_FIELD_NOT_SET => 'Accessing a non-set field',
+        self::ERR_SERVICE_ERROR => 'service error',
+        self::ERR_UI_INPUT_ERROR => 'UI input error',
     );
 
     public static function getErrmsg($errcode, $delimiter = '') {
